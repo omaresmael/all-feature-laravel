6a1ed2e37c5c0d084c07ffc6b7f4e92a845079e3
omaresmael <omaresamel590@gmail.com>
2021-09-21 22:56:24 +0200

    Intilize resources

---
+++ app/Models/Reservation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    use HasFactory;
+
+    protected $casts = [
+      'price' => 'integer',
+      'status' => 'integer',
+      'start_date' => 'immutable_date',
+      'end_date' => 'immutable_date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function office()
+    {
+        return $this->belongsTo(Office::class);
+    }
+}
---
+++ database/factories/ReservationFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Office;
+use App\Models\Reservation;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ReservationFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Reservation::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'office_id' => Office::factory(),
+            'price' => $this->faker->numberBetween(10000,20000),
+            'status' => 1,
+            'start_date' => now()->addDays(1)->format('Y-m-d'),
+            'end_date' => now()->addDays(5)->format('Y-m-d'),
+
+        ];
+    }
+}
